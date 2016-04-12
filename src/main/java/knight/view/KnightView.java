52c1ef58783a39a7200e522cc262048607504b2a
Beke Zoltán <Beke Zoltn>
2016-04-12 16:54:24 +0200

    Knight class implemented in Main.java.

--- src/main/java/knight/view/KnightView.java
+++ src/main/java/knight/view/KnightView.java
@@ -2,8 +2,14 @@ package knight.view;
 
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import knight.model.KnightModel;
 
@@ -13,15 +19,16 @@ import knight.model.KnightModel;
  */
 public class KnightView extends JFrame {
 
+	private KnightModel km;
 	private JButton[][] buttons = new JButton[5][5];
+	private JPanel panel;
 
 	public KnightView() {
 		setLocationRelativeTo(null);
 		setTitle("Knight");
 		setSize(400, 400);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-		JPanel panel = new JPanel();
+		panel = new JPanel();
 		panel.setLayout(new GridLayout(5, 5, 1, 1));
 
 		for (int i = 0; i < 5; i++) {
@@ -34,12 +41,37 @@ public class KnightView extends JFrame {
 			}
 		}
 
-		new KnightModel(getButtons());
+		JMenuBar menubar = new JMenuBar();
+        JMenu newgame = new JMenu("File");
+        menubar.add(newgame);
+
+        JMenuItem restart = new JMenuItem("Restart");
+
+        newgame.add(restart);
+        
+        restart.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				reset();
+				km = new KnightModel(getButtons());
+			}
+		});
+
+        setJMenuBar(menubar);
+		
+		km = new KnightModel(getButtons());
 
 		add(panel);
 	}
 
 	JButton[][] getButtons() {
 		return buttons;
+	}
+	
+	public void reset() {
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				buttons[i][j].setBackground(Color.WHITE);
+			}
+		}
 	}
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,12 +1,16 @@
 package main;
 
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
+import knight.Knight;
 import motor.Motor;
 
 public class Main extends JFrame{
@@ -14,17 +18,22 @@ public class Main extends JFrame{
 	public Main() {
 		
 		JPanel panel = new JPanel() {};
+		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
 		
-		setTitle("Projekt eszközök beadandó");
+		setTitle("Project Tools");
         setSize(480, 270);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(false);
         setLocationRelativeTo(null);
         
-        JButton button1 = new JButton("Fénymotor Párbaj");
-        JButton button2 = new JButton("24x24");
+        JButton button1 = new JButton("Tron(ish) game");
+        button1.setAlignmentX(Component.CENTER_ALIGNMENT);
+        JButton button2 = new JButton("Knight");
+        button2.setAlignmentX(Component.CENTER_ALIGNMENT);
         JButton button3 = new JButton("36x36");
+        button3.setAlignmentX(Component.CENTER_ALIGNMENT);
         JButton button4 = new JButton("4");
+        button4.setAlignmentX(Component.CENTER_ALIGNMENT);
         
         panel.add(button1);
         panel.add(button2);
@@ -38,15 +47,26 @@ public class Main extends JFrame{
 				try {
 					Motor.main(new String[0]);
 				} catch (IOException e) {
-					System.out.println("An error occured while starting Motor class!");
+					System.out.println("An error occured while starting Tron(ish) game!");
 				}
 			}
 		};
 		
+		final Runnable knightRun = new Runnable() {
+			public void run() {
+				Knight.main(new String[0]);
+			}
+		};
+		
 		button1.addActionListener(new ActionListener() {
-			
 			public void actionPerformed(ActionEvent e) {
 				new Thread(motorRun).start();
+			}
+		});
+		
+		button2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				new Thread(knightRun).start();
 			}
 		});
         
